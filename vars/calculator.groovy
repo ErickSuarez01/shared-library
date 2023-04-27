34a6c7d92aa86ab2e63efce1bfa053f340fb9b7e
ErickSuarez01 <129386658+ErickSuarez01@users.noreply.github.com>
2023-04-27 11:38:29 +0200

    Update and rename calculator.groovy to unitTest.groovy

--- vars/calculator.groovy
+++
@@ -1,8 +0,0 @@
-def add (x,y){
-  echo "sum of ${x} and ${y} is ${x+y}"
-  
-}
-def mult (x,y){
-  echo "sum of ${x} and ${y} is ${x*y}"
-  
-}
---
+++ vars/unitTest.groovy
@@ -0,0 +1,4 @@
+def call (){
+  echo"Testing Proyect..."
+  bat"mvn test"
+}
